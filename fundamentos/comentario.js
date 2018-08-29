9c1cd43bcfdcd5b1e5203379015a2109093e55fa
jorgemax94 <37005420+jorgemax94@users.noreply.github.com>
2018-08-29 16:46:18 -0300

    contantes e variaveis

---
+++ fundamentos/comentario.js
@@ -0,0 +1,18 @@
+// Comentario de 1 linha
+console.log('linha 1')
+
+/* comentario
+de varias linhas
+*/
+
+Console.log('Linha 2')
+
+/*
+*
+*comentario
+*de
+*varias linhas
+*
+*/
+
+console.log('Linha 3')
---
+++ fundamentos/constEvar.js
@@ -0,0 +1,11 @@
+var a = 3
+let b = 4
+
+// optar pela declaração com let
+
+console.log(a, b)
+
+
+const c = 5
+
+console.log(c)
