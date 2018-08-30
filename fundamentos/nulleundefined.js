3e00829c6edb34372f68150c22262c472ff393e9
jorgemax94 <37005420+jorgemax94@users.noreply.github.com>
2018-08-30 18:17:01 -0300

    null

---
+++ fundamentos/nulleundefined.js
@@ -0,0 +1,31 @@
+//const b = a atribuição por referência, ambos sofrem alterações juntos
+
+/*
+atribuição por referência = atribuição de objetos
+atribuição por valores = atribuição de valores primitivos
+*/
+
+
+let valor // não inicializada
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+// console.log(valor.toString()) não é possível referenciar algo nulo
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined // evite atribuir undefined
+console.log(!!produto.preco)
+//delete produto.preco para remover valor da variavel
+console.log(produto)
+
+
+produto.preco = null // sem preço
+console.log(!!produto.preco)
+console.log(produto)
---
+++ fundamentos/objetos.js
@@ -0,0 +1,14 @@
+const prod1 = {} // representa a forma literal de um objeto , 'identificador'{} par de chaves
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4898.99
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+
+}
+
+console.log(prod2)
