1dd461cdcdd0de4a3bb6bc4a3e94e0e1a49004f8
Jorge Max <juninbone@gmail.com>
2018-10-04 18:19:00 -0300

    controles2

---
+++ controle/doWhile.js
@@ -0,0 +1,15 @@
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random() * (max -min) + min
+    console.log('valor randominco ' + valor)
+    return Math.floor(valor)
+}
+
+
+let opcao = 0
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}while(opcao != -1)
+
+console.log('Até a próxima!')
